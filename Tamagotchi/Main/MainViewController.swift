9fd2d2e72a796996bc0d33d8277f95eacdcc2c40
LEESANGNAM <fgh0116@naver.com>
2023-08-05 14:50:50 +0900

    메인화면 UI 구현

---
+++ Tamagotchi/Main/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  Tamagotchi
+//
+//  Created by 이상남 on 2023/08/05.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
