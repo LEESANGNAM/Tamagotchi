0db5c89fef66198e9109b850bc7058626cd7b0fc
LEESANGNAM <fgh0116@naver.com>
2023-08-06 01:51:03 +0900

    setting화면 추가

---
+++ Tamagotchi/Setting/SettingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SettingTableViewCell.swift
+//  Tamagotchi
+//
+//  Created by 이상남 on 2023/08/06.
+//
+
+import UIKit
+
+class SettingTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var settingLeftIamge: UIImageView!
+    @IBOutlet weak var settingLabel: UILabel!
+    @IBOutlet weak var settingNickNameLabel: UILabel!
+    @IBOutlet weak var settingRightIamge: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+}
---
+++ Tamagotchi/Setting/SettingViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SettingViewController.swift
+//  Tamagotchi
+//
+//  Created by 이상남 on 2023/08/06.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    @IBOutlet weak var settingTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
