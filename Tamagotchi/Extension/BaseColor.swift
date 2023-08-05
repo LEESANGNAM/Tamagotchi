2fede40db0ac950546d41c2035d31e3b75b796e4
LEESANGNAM <fgh0116@naver.com>
2023-08-05 17:31:18 +0900

    사용하는(배경,폰트,테두리)색상 파일추가

---
+++ Tamagotchi/Extension/BaseColor.swift
@@ -0,0 +1,14 @@
+//
+//  Extension+UIColor.swift
+//  Tamagotchi
+//
+//  Created by 이상남 on 2023/08/05.
+//
+
+import UIKit
+
+struct BaseColor{
+    static let backgroundColor = UIColor(red: 245/255, green: 252/255, blue: 252/255, alpha: 1)
+    static let fontColor =  UIColor(red: 77/255, green: 106/255, blue: 120/255, alpha: 1)
+    static let borderColor = CGColor(red: 77/255, green: 106/255, blue: 120/255, alpha: 1)
+}
