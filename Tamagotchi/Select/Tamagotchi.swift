97e52ec51ad37bde07f0dc0284e0bf6e2427f878
LEESANGNAM <fgh0116@naver.com>
2023-08-05 12:12:23 +0900

    다마고치 설명 추가

--- Tamagotchi/Select/Tamagotchi.swift
+++ Tamagotchi/Select/Tamagotchi.swift
@@ -40,6 +40,18 @@ enum TamagotchiType {
         }
     }
     
+    var contens: String{
+        switch self{
+        case .ttakkeum:
+            return "저는 선인장 다마고치입니다. 키는 2cm 몸무게는 150g 이에요. 성격은 소심하지만 마음은 따뜻해요. 열심히 잘 먹고 잘 클 자신은 있답니다. 반가워요 주인님~:)"
+        case .bangsil:
+            return "저는 방실방실 다마고치입니당! 키는 100km 몸무게는 150톤이에용! 성격은 화끈하고 날라다닙니당! 열심히 잘 먹고 잘 클 자신은 있답니당! 방실방실! "
+        case .banjjag:
+            return "저는 반짝반짝 다마고치에요~ 키는 120cm 몸무게는 120g이에요~ 성격도 반짝반짝 시원시원 해요~ 저를 키워주세요~ 잘 먹고 잘 커볼게요~"
+        case .none:
+            return " 준비중이에요 ~ 조금만 기다려주세요! 곧 찾아 오겠습니다 :)"
+        }
+    }
     
 }
 
