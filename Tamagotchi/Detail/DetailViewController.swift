293a84083e19c744009b0ee9024e055dc8f53e2c
LEESANGNAM <fgh0116@naver.com>
2023-08-05 12:33:46 +0900

    상세 팝업 화면 UI 구현

--- Tamagotchi/Detail/DetailViewController.swift
+++ Tamagotchi/Detail/DetailViewController.swift
@@ -9,23 +9,37 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet weak var detailImageView: UIImageView!
+    @IBOutlet weak var detailNameLabel: UILabel!
+    @IBOutlet weak var detailContentsLabel: UILabel!
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var selectButton: UIButton!
+    
+    @IBOutlet weak var backView: UIView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUpUI()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setUpUI(){
+        
+        backView.backgroundColor = .gray
+        
+        detailImageView.layer.cornerRadius = detailImageView.bounds.width / 2
+        detailImageView.layer.masksToBounds = true
+        
+        detailImageView.backgroundColor = .blue
+        
+        detailNameLabel.text = "다마고치 이름자리"
+        detailContentsLabel.text = TamagotchiType.none.contens
+        
+        
+        cancelButton.titleLabel?.text = "취소"
+        selectButton.titleLabel?.text = "시작하기"
     }
-    */
 
 }
