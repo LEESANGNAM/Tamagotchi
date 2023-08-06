a0d470a66b35a4af06572f1a43216199798e70a7
LEESANGNAM <fgh0116@naver.com>
2023-08-06 13:21:40 +0900

    닉네임변경(SettingNameView) 추가

---
+++ Tamagotchi/Setting/SettingNameViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SettingNameViewController.swift
+//  Tamagotchi
+//
+//  Created by 이상남 on 2023/08/06.
+//
+
+import UIKit
+
+class SettingNameViewController: UIViewController {
+
+    
+    @IBOutlet weak var nameSettingTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+
+}
