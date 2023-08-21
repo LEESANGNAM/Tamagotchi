6a4f4e4b7c2c74db4cfe03640fe2f460289dcad6
LEESANGNAM <fgh0116@naver.com>
2023-08-22 01:33:35 +0900

    dailyNotification 추가

--- Tamagotchi/AppDelegate.swift
+++ Tamagotchi/AppDelegate.swift
@@ -14,9 +14,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UNUserNotificationCenter.current().delegate = self
+        
+        // 알림 권한 설정
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.badge]) { success, error in
+            if success{
+                self.dailyNineTimeNotification()
+                print("성공한건가")
+            }else{
+                print(error)
+            }
+        }
         return true
     }
 
+    func dailyNineTimeNotification(){
+        // 포그라운드에서 알림이 안뜨는게 디폴트
+        // 알림보내!
+        //1. 컨텐츠 2. 언제
+        
+        //1. 컨텐츠 채우기
+        let content = UNMutableNotificationContent()
+        content.title = "다마고치에게 물을 \(Int.random(in: 1...49))모금주세요"
+        content.body = "목말라요. 물줘요"
+        content.badge = 1
+        
+        var component = DateComponents() //구조체라 var
+        component.minute = 0 // 5분
+        component.hour = 9
+        //생일 적어놓고 생일이라고 보내기, 새해 날짜 정해놓고 보내기, 크리스마스 날짜 정해놓고 보내기 등등
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: component, repeats: true)
+        
+        //2. 언제
+        let request = UNNotificationRequest(identifier: "dailyNineTimeNotification", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            print(error)
+        }
+    }
+    
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -32,5 +70,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
 
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        // 특정화면, 특정조건에서만 포그라운드 알림 받기,
+        // 특정 화면에서는 알림 안받기
+        completionHandler([.sound,.badge,.banner,.list])
+    }
 }
 
